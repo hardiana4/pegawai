ba1e6d95bc8fdf2b4ba0e95a0807a4e75f2c30aa
Hardiana <87222054+hardiana4@users.noreply.github.com>
2022-08-25 14:17:35 +0700

    Add files via upload

---
+++ footer.blade.php
@@ -0,0 +1,21 @@
+</div>
+<!-- /.content-wrapper -->
+<div class="box-header text-center">
+    <footer class="main-footer">
+    <a>copyright &copy; 2022</a><strong class="text-danger">Diskominfo Cilacap</strong>
+    </footer>
+</div>
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+  <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="{{asset('template')}}/plugins/jquery/jquery.min.js"></script>
+<script src="{{asset('template')}}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{asset('template')}}/dist/js/adminlte.min.js"></script>
+
